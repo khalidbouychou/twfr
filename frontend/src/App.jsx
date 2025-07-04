612d9fd96e3c105200fe7694148587548da43145
unknown <khalidbouychou@users.noreply.github.com>
2025-07-04 17:08:32 +0100

    setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+
+  return (
+
+      <div className="bg-red-500 h-screen flex  justify-center items-center">
+        <h1 className="text-2xl font-bold"> hello </h1>
+      </div>
+  );
+}
+
+export default App;
